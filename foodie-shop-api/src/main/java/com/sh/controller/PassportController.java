c228be88b16adf89d64ee2c9ef8da2cef2fbb933
leeshuhua <leeshuhua@163.com>
2020-01-31 16:51:16 +0800

    实现用户登录及页面显示

--- foodie-shop-api/src/main/java/com/sh/controller/PassportController.java
+++ foodie-shop-api/src/main/java/com/sh/controller/PassportController.java
@@ -1,7 +1,11 @@
 package com.sh.controller;
 
+import com.sh.pojo.Users;
 import com.sh.pojo.bo.UserBO;
 import com.sh.service.UserService;
+import com.sh.utils.CookieUtils;
+import com.sh.utils.JsonUtils;
+import com.sh.utils.MD5Utils;
 import com.sh.utils.MyJSONResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -9,6 +13,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @Author lee
@@ -77,4 +83,51 @@ public class PassportController {
 
         return MyJSONResult.ok();
     }
+
+    @ApiOperation(value = "用户登录", notes = "用户登录", httpMethod = "POST")
+    @PostMapping("/login")
+    public MyJSONResult login(@RequestBody UserBO userBO,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response) throws Exception {
+
+        String username = userBO.getUsername();
+        String password = userBO.getPassword();
+
+        // 0. 判断用户名和密码必须不为空
+        if (StringUtils.isBlank(username) ||
+                StringUtils.isBlank(password)) {
+            return MyJSONResult.errorMsg("用户名或密码不能为空");
+        }
+
+        // 1. 实现登录
+        Users userResult = userService.queryUserForLogin(username,
+                MD5Utils.getMD5Str(password));
+
+        if (userResult == null) {
+            return MyJSONResult.errorMsg("用户名或密码不正确");
+        }
+
+        userResult = setNullProperty(userResult);
+
+        // 设置COOKIE
+        CookieUtils.setCookie(request, response, "user",
+                JsonUtils.objectToJson(userResult), true);
+
+
+        // TODO 生成用户token，存入redis会话
+        // TODO 同步购物车数据
+
+        return MyJSONResult.ok(userResult);
+    }
+
+    private Users setNullProperty(Users userResult) {
+        userResult.setPassword(null);
+        userResult.setMobile(null);
+        userResult.setEmail(null);
+        userResult.setCreatedTime(null);
+        userResult.setUpdatedTime(null);
+        userResult.setBirthday(null);
+        return userResult;
+    }
+
 }
