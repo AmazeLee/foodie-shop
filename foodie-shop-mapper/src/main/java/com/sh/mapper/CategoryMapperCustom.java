cf8bdc5f348e0b52d40c77d432e1870e33952bb5
leeshuhua <leeshuhua@163.com>
2020-03-03 23:09:12 +0800

    加载与渲染大分类，自连接查询子分类，懒加载子分类

---
+++ foodie-shop-mapper/src/main/java/com/sh/mapper/CategoryMapperCustom.java
@@ -0,0 +1,15 @@
+package com.sh.mapper;
+
+import com.sh.pojo.vo.CategoryVO;
+import com.sh.pojo.vo.NewItemsVO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CategoryMapperCustom {
+
+    public List<CategoryVO> getSubCatList(Integer rootCatId);
+
+    public List<NewItemsVO> getSixNewItemsLazy(@Param("paramsMap") Map<String, Object> map);
+}
---
+++ foodie-shop-service/src/main/java/com/sh/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,56 @@
+package com.sh.service.impl;
+
+import com.sh.mapper.CategoryMapper;
+import com.sh.mapper.CategoryMapperCustom;
+import com.sh.pojo.Category;
+import com.sh.pojo.vo.CategoryVO;
+import com.sh.pojo.vo.NewItemsVO;
+import com.sh.service.CategoryService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Resource
+    private CategoryMapper categoryMapper;
+
+    @Resource
+    private CategoryMapperCustom categoryMapperCustom;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<Category> queryAllRootLevelCat() {
+
+        Example example = new Example(Category.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("type", 1);
+
+        List<Category> result =  categoryMapper.selectByExample(example);
+
+        return result;
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<CategoryVO> getSubCatList(Integer rootCatId) {
+        return categoryMapperCustom.getSubCatList(rootCatId);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<NewItemsVO> getSixNewItemsLazy(Integer rootCatId) {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("rootCatId", rootCatId);
+
+        return categoryMapperCustom.getSixNewItemsLazy(map);
+    }
+}
