5b293424a8e55214f84af8bcae8a3e94ad3b4277
leeshuhua <leeshuhua@163.com>
2020-04-04 23:27:45 +0800

    删除商品

--- foodie-shop-api/src/main/java/com/sh/controller/ShopCartController.java
+++ foodie-shop-api/src/main/java/com/sh/controller/ShopCartController.java
@@ -42,4 +42,22 @@ public class ShopCartController {
 
         return MyJSONResult.ok();
     }
+
+    @ApiOperation(value = "从购物车中删除商品", notes = "从购物车中删除商品", httpMethod = "POST")
+    @PostMapping("/del")
+    public MyJSONResult del(
+            @RequestParam String userId,
+            @RequestParam String itemSpecId,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+
+        if (StringUtils.isBlank(userId) || StringUtils.isBlank(itemSpecId)) {
+            return MyJSONResult.errorMsg("参数不能为空");
+        }
+
+        // TODO 用户在页面删除购物车中的商品数据，如果此时用户已经登录，则需要同步删除后端购物车中的商品
+
+        return MyJSONResult.ok();
+    }
 }
